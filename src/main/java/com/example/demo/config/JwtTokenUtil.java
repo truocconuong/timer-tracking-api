1941e76c9f73fd41b3243cfc2a0ede83ec9a7dda
Manh Chu <chuhuumanh331997@gmail.com>
2020-09-06 10:32:20 +0700

    tracking is real

--- src/main/java/com/example/demo/config/JwtTokenUtil.java
+++ src/main/java/com/example/demo/config/JwtTokenUtil.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Component
 public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007487L;
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 12000 * 60 * 60;
 
     @Value("${jwt.secret}")
     private String secret;
